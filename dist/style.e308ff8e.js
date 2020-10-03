386e30f331d891e020ce57b67186a05882383f58
TQ <756342530@qq.com>
2020-10-03 15:59:53 +0800

    init

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1990" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1116" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
